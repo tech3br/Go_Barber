e67221279152271f82620a185f1fc86827d5cdff
Daniel Filgueira <dfsdireito95@gmail.com>
2020-11-18 18:56:49 -0300

    Adding unform and changes in input

--- frontend-go-barber/src/pages/SignUp/index.tsx
+++ frontend-go-barber/src/pages/SignUp/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
+import { Form } from '@unform/web';
 
 import logoImg from '../../assets/logo.svg';
 
@@ -9,18 +10,36 @@ import Button from '../../components/Button';
 
 import * as S from './styles';
 
+type HandleSubmitProps = { name: string; email: string; password: string };
+
 const SignUp: React.FC = () => {
+  const handleSubmit = (data: HandleSubmitProps): void => {
+    console.log(data);
+  };
+
   return (
     <S.Container>
       <S.Background />
       <S.Content>
         <S.Logo src={logoImg} />
-        <form>
+
+        <Form onSubmit={handleSubmit}>
           <h1>Faça seu cadastro</h1>
 
-          <Input name="name" icon={FiUser} placeholder="Nome" />
-          <Input name="email" icon={FiMail} placeholder="Email" />
           <Input
+            autoComplete="current-name"
+            name="name"
+            icon={FiUser}
+            placeholder="Nome"
+          />
+          <Input
+            autoComplete="current-email"
+            name="email"
+            icon={FiMail}
+            placeholder="Email"
+          />
+          <Input
+            autoComplete="current-password"
             name="password"
             icon={FiLock}
             type="password"
@@ -28,7 +47,8 @@ const SignUp: React.FC = () => {
           />
 
           <Button type="submit">Cadastrar</Button>
-        </form>
+        </Form>
+
         <a href="login">
           <FiArrowLeft />
           Voltar para login
