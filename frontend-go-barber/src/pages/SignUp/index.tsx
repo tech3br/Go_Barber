9cef84b8b152665b470a50bd06bb86f2a5c45ab9
Daniel Filgueira <dfsdireito95@gmail.com>
2020-11-20 23:36:29 -0300

    Adding yup and eslint disable messages

--- frontend-go-barber/src/pages/SignUp/index.tsx
+++ frontend-go-barber/src/pages/SignUp/index.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
+/* eslint-disable no-console */
+import React, { useCallback } from 'react';
 import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
 import { Form } from '@unform/web';
+import * as Yup from 'yup';
 
 import logoImg from '../../assets/logo.svg';
 
@@ -13,9 +15,26 @@ import * as S from './styles';
 type HandleSubmitProps = { name: string; email: string; password: string };
 
 const SignUp: React.FC = () => {
-  const handleSubmit = (data: HandleSubmitProps): void => {
-    console.log(data);
-  };
+  const handleSubmit = useCallback(async (data: HandleSubmitProps) => {
+    try {
+      const schema = Yup.object().shape({
+        name: Yup.string().required('O nome é obrigatório'),
+        email: Yup.string()
+          .required('O email é obrigatório')
+          .email('Digite um email válido'),
+        password: Yup.string().min(
+          8,
+          'A senha deve ter no mínimo 8 caracteres',
+        ),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
 
   return (
     <S.Container>
