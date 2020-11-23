fd9cb7ebdd2f493701afc497e9ecb3c021809c73
Daniel Filgueira <dfsdireito95@gmail.com>
2020-11-22 21:45:53 -0300

    Adding dashboard component and Route component

---
+++ frontend-go-barber/src/pages/Dashboard/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Dashboard: React.FC = () => {
+  return <h1 style={{ color: '#fff' }}>Dashboard</h1>;
+};
+
+export default Dashboard;
--- frontend-go-barber/src/routes/index.tsx
+++ frontend-go-barber/src/routes/index.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
-
+import { Switch } from 'react-router-dom';
+import Dashboard from '../pages/Dashboard';
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
+import Route from './Route';
 
 const Routes: React.FC = () => {
   return (
     <Switch>
       <Route path="/" exact component={SignIn} />
-      <Route path="/signup" exact component={SignUp} />
+      <Route path="/signup" component={SignUp} />
+
+      <Route path="/dashboard" component={Dashboard} />
     </Switch>
   );
 };
