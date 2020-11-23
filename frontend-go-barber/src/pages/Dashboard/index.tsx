d17c6a756d6ac4eee52a001ee4adcdf47b1465d5
Daniel Filgueira <dfsdireito95@gmail.com>
2020-11-23 01:05:33 -0300

    Adding button logout from context useAuth

--- frontend-go-barber/src/pages/Dashboard/index.tsx
+++ frontend-go-barber/src/pages/Dashboard/index.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { getJSDocReturnType } from 'typescript';
+import { useAuth } from '../../hooks/auth';
 
 const Dashboard: React.FC = () => {
+  const { signOut } = useAuth();
+
   return (
     <div>
       <h1 style={{ color: '#fff' }}>Dashboard</h1>
+      <button type="submit" onClick={signOut}>
+        Sair
+      </button>
     </div>
   );
 };
